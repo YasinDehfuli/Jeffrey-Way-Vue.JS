9dd8fa706f77b97da6a9183079291be9ef4b484d
YasinDehfuli <ydehfuli@gmail.com>
2023-09-29 15:59:29 +0330

    Create a component & Export it from AppButton.js and import it to step-5.html - ( Add Google Extension: Vue.js DevTool )

---
+++ components/AppButton.js
@@ -0,0 +1,19 @@
+export default {
+
+    template: `
+        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+            <slot></slot>
+        </button>
+        `,
+
+
+    data() {
+        return {
+            processing: true
+        };
+    },
+
+    mounted() {
+        alert('hello')
+    }
+}
