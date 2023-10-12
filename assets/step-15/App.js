507b7aeea83425b303e7f20d1b156287ed749110
YasinDehfuli <ydehfuli@gmail.com>
2023-10-12 18:15:57 +0330

    Add step-15.html & App.js

---
+++ assets/step-15/App.js
@@ -0,0 +1,14 @@
+    import Assignments from "./Assignments.js";
+    import Panel from "./Panel.js";
+export default {
+    components:{ Assignments, Panel },
+    template: `
+        <div class="grid gap-6">
+            <assignments></assignments>
+          
+        </div>
+ 
+    `,
+
+
+}
